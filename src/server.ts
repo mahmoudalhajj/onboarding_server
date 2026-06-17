41ff324c71f88d3b5b8685d57a209e777dca0aa1
mahmoud alhajj <mahmoud.alhajj@et3arraf.com>
2026-06-17 15:15:49 +0300

    initial folder structure

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/health", (req, res) => {
+  res.status(200).json({
+    status: "ok",
+    uptime: process.uptime(),
+    timestamp: Date.now(),
+  });
+});
+
+app.listen(PORT,()=>{
+  console.log(`running on port ${PORT}`);
+});
